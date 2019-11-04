77009222f83ab663a9192e5bd456ec28e5d9c787
hyeonhyejin <dev.hyejin04@gmail.com>
2019-11-04 20:59:00 +0900

    문제에 대한 문서 작성.

--- 1algorithm-1day/20191104/src/main/kotlin/com/hyejineee/passingTruck/passingTruck.kt
+++ 1algorithm-1day/20191104/src/main/kotlin/com/hyejineee/passingTruck/passingTruck.kt
@@ -1,5 +1,12 @@
 package com.hyejineee.passingTruck
 
-fun passingTruck():Int{
+import java.util.*
+
+fun passingTruck(bridge_length: Int, weight: Int, truck_weights: IntArray):Int{
+    var bridge_stack = Stack<Int>()
+    //1. 다리 위의 트럭의 무게가 10 미만인 경우에만 다리에 트럭을 하나씩 올린다.
+    bridge_stack.push(truck_weights[0])
+    if(bridge_stack.sum() <10 ) bridge_stack.push(truck_weights[1])
+
     return 0
 }

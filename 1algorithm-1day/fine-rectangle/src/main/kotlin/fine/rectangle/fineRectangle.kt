561046dc16acf8014f960a00adbf4f19738fe8f2
hannut91 <hannut91@gmail.com>
2019-12-24 17:22:12 +0900

    Refactor solution of findRectangle
    Use `find` instead of for loop.

--- 1algorithm-1day/fine-rectangle/src/main/kotlin/fine/rectangle/fineRectangle.kt
+++ 1algorithm-1day/fine-rectangle/src/main/kotlin/fine/rectangle/fineRectangle.kt
@@ -1,23 +1,18 @@
 package fine.rectangle
 
-fun fineRectangle(w: Int, h: Int): Long = w.toLong() * h.toLong() - wrongRectanglesCount(w, h)
+fun fineRectangle(w: Int, h: Int) =
+        w.toLong() * h.toLong() - wrongRectanglesCount(w, h)
 
 fun wrongRectanglesCount(w: Int, h: Int): Long {
     val g = gcd(w, h)
     return g * (w / g + h / g - 1).toLong()
 }
 
-fun gcd(num1: Int, num2: Int): Int = maxCommon(calculateFactors(num1), calculateFactors(num2))
+fun gcd(num1: Int, num2: Int) =
+        maxCommon(calculateFactors(num1), calculateFactors(num2))
 
-fun maxCommon(numbers1: Array<Int>, numbers2: Array<Int>): Int {
-    for (i in numbers1.reversedArray()) {
-        if (i in numbers2) {
-            return i
-        }
-    }
-    return 1
-}
+fun maxCommon(numbers1: Array<Int>, numbers2: Array<Int>) =
+        numbers1.reversedArray().find { it in numbers2 } ?: 1
 
-fun calculateFactors(num: Int): Array<Int> = (1..num).filter { num % it == 0 }.toTypedArray()
-
-
+fun calculateFactors(num: Int) =
+        (1..num).filter { num % it == 0 }.toTypedArray()

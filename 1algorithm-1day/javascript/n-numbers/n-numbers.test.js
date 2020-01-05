3eb48a20ad47eb61173bb3a492e1756337accdbe
hyeonhyejin <dev.hyejin04@gmail.com>
2020-01-05 22:41:31 +0900

    Solve n-numbers problem by Javascript
    Solve n-numbers problem by Javascript
    
    See also
      - https://programmers.co.kr/learn/courses/30/lessons/42583

---
+++ 1algorithm-1day/javascript/n-numbers/n-numbers.test.js
@@ -0,0 +1,9 @@
+const nNumbers = (x, n)=>{
+    return Array.from({length: n},(v,index)=>(index+1)*x);
+};
+
+test(`x만큼 간격이 있는 n개의 수 테스트`,()=>{
+    expect(nNumbers(2,5)).toEqual([2,4,6,8,10]);
+    expect(nNumbers(4,3)).toEqual([4,8,12]);
+    expect(nNumbers(-4,2)).toEqual([-4, -8]);
+})
